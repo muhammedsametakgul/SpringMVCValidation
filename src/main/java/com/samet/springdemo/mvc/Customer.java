7950dfcd1a61f0b6f18dd2977d1c4170bfbf7d97
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-08-01 14:06:29 +0300

    Add @Pattern annotiation

--- src/main/java/com/samet/springdemo/mvc/Customer.java
+++ src/main/java/com/samet/springdemo/mvc/Customer.java
@@ -1,10 +1,7 @@
 package com.samet.springdemo.mvc;
 
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -19,6 +16,18 @@ public class Customer {
     @Min(value = 0,message = "must be greater than or equal to zero")
     @Max(value = 10,message = "must be less than or equal to ten")
     private int freePasses;
+
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}" , message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return freePasses;
