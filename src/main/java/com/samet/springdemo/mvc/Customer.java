d7e7d348ab173bfa5773a08cf4cb14e424cbe95c
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-08-01 14:13:53 +0300

    Create messages.properties and make custom error mistakes

--- src/main/java/com/samet/springdemo/mvc/Customer.java
+++ src/main/java/com/samet/springdemo/mvc/Customer.java
@@ -13,9 +13,10 @@ public class Customer {
     private String lastName;
 
 
+    @NotNull(message = "is required")
     @Min(value = 0,message = "must be greater than or equal to zero")
     @Max(value = 10,message = "must be less than or equal to ten")
-    private int freePasses;
+    private Integer freePasses;
 
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}" , message = "only 5 chars/digits")
@@ -29,11 +30,11 @@ public class Customer {
         this.postalCode = postalCode;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
