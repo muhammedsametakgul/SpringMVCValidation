9780a22b121e1138ba028fe573b77fcf1ed3db96
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-08-01 13:43:25 +0300

    Add @InitBinder support to remove whitespace

--- src/main/java/com/samet/springdemo/mvc/CustomerController.java
+++ src/main/java/com/samet/springdemo/mvc/CustomerController.java
@@ -2,15 +2,26 @@ package com.samet.springdemo.mvc;
 
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    //add an initbinder ........to convert trim input strings
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
@@ -27,6 +38,8 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult)
     {
+        System.out.println("Last Name : |" + theCustomer.getLastName()+ " |");
+
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }else{
