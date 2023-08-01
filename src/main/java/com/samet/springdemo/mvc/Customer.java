ff7b228d50ecdf67a12992b52a0652901dc741ef
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-08-01 13:53:59 +0300

    Add @Max and @Min support for numerical input

--- src/main/java/com/samet/springdemo/mvc/Customer.java
+++ src/main/java/com/samet/springdemo/mvc/Customer.java
@@ -1,6 +1,8 @@
 package com.samet.springdemo.mvc;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,6 +14,19 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1,message = "is required")
     private String lastName;
+
+
+    @Min(value = 0,message = "must be greater than or equal to zero")
+    @Max(value = 10,message = "must be less than or equal to ten")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
